2dbd712adaf55d01f1e4a4f79f036977e06347f6
yangzecai <879894445@qq.com>
2021-12-27 23:13:02 +0800

    implement wrap and unwrap between seqno and absolute seqno

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -6,7 +6,7 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32(static_cast<uint32_t>(n & 0xffffffff) + isn.raw_value());
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,19 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t base = n.raw_value() - isn.raw_value();
+    uint64_t first = (checkpoint >> 32) << 32 | base;
+    uint64_t second = max(((checkpoint >> 32) + 1) << 32 | base, first);
+    uint64_t third = min(((checkpoint >> 32) - 1) << 32 | base, first);
+    uint64_t first_dist = min(checkpoint - first, first - checkpoint);
+    uint64_t second_dist = min(checkpoint - second, second - checkpoint);
+    uint64_t third_dist = min(checkpoint - third, third - checkpoint);
+    uint64_t min_dist = min(first_dist, min(second_dist, third_dist));
+    if (min_dist == first_dist) {
+        return first;
+    } else if (min_dist == second_dist) {
+        return second;
+    } else {
+        return third;
+    }
 }
